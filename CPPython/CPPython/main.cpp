e9b629c2f557165f1cccc5661ab6e7af36875afe
Brandon Littell <brandonl3737@gmail.com>
2014-04-08 15:44:18 -0400

    Added all of python and included it.

--- CPPython/CPPython/main.cpp
+++ CPPython/CPPython/main.cpp
@@ -1,8 +1,18 @@
+#ifdef _DEBUG
+#undef _DEBUG
+#endif //_DEBUG
+#include <Python.h>
+#ifndef _DEBUG
+#define _DEBUG
+#endif //_DEBUG
+
+#include <stdio.h>
 
 
 int main( int argc, char *argv[] )
 {
-
+	printf("Hello, world!");
+	
 
 	return 0;
 }
