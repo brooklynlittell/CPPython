917055036555e7129bf854a1c8c25545f262972d
Brandon Littell <brandonl3737@gmail.com>
2014-04-14 17:57:15 -0400

    Python loads a script via command line.

--- CPPython/CPPython/main.cpp
+++ CPPython/CPPython/main.cpp
@@ -5,13 +5,13 @@
 #ifndef _DEBUG
 #define _DEBUG
 #endif //_DEBUG
-
+/*
 #include <stdio.h>
 
 
 int main( int argc, char *argv[] )
 {
-	Py_SetProgramName(argv[0]);  /* optional but recommended */
+	Py_SetProgramName(argv[0]);  // optional but recommended 
 	Py_Initialize();
 	PyRun_SimpleString("from time import time,ctime\n"
 		"print 'Today is',ctime(time())\n");
@@ -19,5 +19,96 @@ int main( int argc, char *argv[] )
 	
 	system("pause");
 
+	return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int myInt = 0;
+
+	PyObject *pName, *pModule, *pDict, *pFunc;
+	PyObject *pArgs, *pValue;
+	int i;
+
+	if (argc < 3) {
+		fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
+		return 1;
+	}
+
+	Py_Initialize();
+	pName = PyString_FromString(argv[1]);
+	/* Error checking of pName left out */
+
+	pModule = PyImport_Import(pName);
+	Py_DECREF(pName);
+
+	if (pModule != NULL) {
+		pFunc = PyObject_GetAttrString(pModule, argv[2]);
+		/* pFunc is a new reference */
+
+		if (pFunc && PyCallable_Check(pFunc)) {
+			pArgs = PyTuple_New(argc - 3);
+			for (i = 0; i < argc - 3; ++i) {
+				pValue = PyInt_FromLong(atoi(argv[i + 3]));
+				if (!pValue) {
+					Py_DECREF(pArgs);
+					Py_DECREF(pModule);
+					fprintf(stderr, "Cannot convert argument\n");
+					return 1;
+				}
+				/* pValue reference stolen here: */
+				PyTuple_SetItem(pArgs, i, pValue);
+			}
+			pValue = PyObject_CallObject(pFunc, pArgs);
+			Py_DECREF(pArgs);
+			if (pValue != NULL) {
+				// Float
+				if(PyFloat_Check(pValue))
+				{
+					printf("Result of call float: %f\n", PyFloat_AsDouble(pValue));
+				}
+				// int
+				else if( PyInt_Check(pValue) )
+				{
+					printf("Result of call int: %ld\n", PyInt_AsLong(pValue));
+					myInt = PyInt_AsLong(pValue);
+				}
+				// Everything else
+				else
+				{
+					printf("Result of call string?: %s\n", PyString_AsString(pValue));
+				}
+				
+				Py_DECREF(pValue);
+			}
+			else {
+				Py_DECREF(pFunc);
+				Py_DECREF(pModule);
+				PyErr_Print();
+				fprintf(stderr,"Call failed\n");
+				return 1;
+			}
+		}
+		else {
+			if (PyErr_Occurred())
+				PyErr_Print();
+			fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+		}
+		Py_XDECREF(pFunc);
+		Py_DECREF(pModule);
+	}
+	else {
+		PyErr_Print();
+		fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
+		return 1;
+	}
+	Py_Finalize();
+
+	printf( "Value of myInt: %d\n", myInt);
+
+	system("pause");
 	return 0;
 }
---
+++ CPPython/Debug/pi.py
@@ -0,0 +1,14 @@
+def multiply(a,b):
+    c = 0
+    for i in range(0, a):
+        c = c + b
+    return c
+
+def add(a, b):
+	return a + b
+
+def pi(a,b):
+	return 3.1415926535897932384626
+
+def returnString():
+	return "hello world"
